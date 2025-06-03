3a0d3c7407df149f52db2a49602c0e79abfcf051
Dade1991 <dade91@msn.com>
2025-06-03 16:50:28 +0200

    Added files

---
+++ script.js
@@ -0,0 +1,20 @@
+// JavaScript YT Full course (https://youtu.be/EerdGm-ehJQ?si=-4WnxR2GNVdbaG_u)
+
+// Exercise 1 - min 14:40
+
+const tshirt = 10
+const socks = 8
+const dinnerPlates = 20
+
+let total = tshirt + socks + dinnerPlates
+console.log(total)
+
+const bankAccount = 100
+const launch = 20
+const dinner = 50
+const earn = 200
+
+let total2 = bankAccount - launch - dinner + earn
+console.log(total2)
+
+document.body.innerHTML = ""
